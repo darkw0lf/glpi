fd17f09361d28b7567f744d3af1154d25fecb629
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2026-02-19 10:41:17 +0100

    Add Docker installation script for Debian
    This script installs Docker on a Debian system, sets up the necessary repository, and verifies the installation by running a test container.

---
+++ docker_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt update
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+systemctl status docker
+
+systemctl start docker
+
+systemctl enable docker
+
+docker run hello-world
